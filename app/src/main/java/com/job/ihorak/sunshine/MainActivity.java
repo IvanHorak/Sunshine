e8787dd8f44e6fbf4047f89f30cbeb3a4894a85a
John Curton <ivan-horak@hotmail.com>
2016-02-20 16:30:24 +0100

    Connected ListView with forecastDataAdapter and filled the ListView with some fake data

--- app/src/main/java/com/job/ihorak/sunshine/MainActivity.java
+++ app/src/main/java/com/job/ihorak/sunshine/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
     public static class PlaceholderFragment extends Fragment{
 
         ArrayAdapter<String> forecastDataAdapter;
         List<String> weekForecast;
+        ListView listView;
+        View rootView;
 
 
         public PlaceholderFragment() {
@@ -35,13 +44,12 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
             String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
+                    "Pon 6/23 - Sunčano - 31/17",
+                    "Uto 6/24 - Maglovito - 21/8",
+                    "Sri 6/25 - Oblačno - 22/17",
+                    "Čet 6/26 - Kišovito - 18/11",
+                    "Pet 6/27 - Maglovito - 21/10",
+                    "Sub 6/28 - SNJEŽOVITO - 23/18"
             };
 
             weekForecast = new ArrayList<String>(Arrays.asList(data));
@@ -49,9 +57,14 @@ public class MainActivity extends AppCompatActivity {
             forecastDataAdapter =
                     new ArrayAdapter<String>(
                             getActivity(),
-                            R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+                            R.layout.list_item_forecast,
+                            R.id.list_item_forecast_textview,
+                            weekForecast);
 
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(forecastDataAdapter);
             return rootView;
 
 
